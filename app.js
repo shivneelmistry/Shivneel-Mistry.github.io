edc1c24a730ade49d704f58d3fdb3fa187854668
Shivneel-Mistry <60072746+Shivneel-Mistry@users.noreply.github.com>
2021-06-22 02:08:41 -0400

    Website Files

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const navAnim = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    });
+
+}
+
+
+navAnim();
+
+
